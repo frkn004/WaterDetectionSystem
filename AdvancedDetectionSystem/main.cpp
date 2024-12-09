a1c3988576411eee26de81760c91cf8318591f18
frkn004 <154842424+frkn004@users.noreply.github.com>
2024-12-09 22:12:02 +0300

    Initial Commit

---
+++ AdvancedDetectionSystem/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  AdvancedDetectionSystem
+//
+//  Created by Furkan Sevinç on 9.12.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
